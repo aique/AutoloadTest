f4403e14240d4c2d9cc3d8b524fc0a89d788be66
aique <aique@q-interactiva.com>
2012-03-28 13:12:18 +0200

    primera version

--- library/qframe/file/FileUtil.php
+++ library/qframe/file/FileUtil.php
@@ -1,12 +1,69 @@
 <?php
 
+/**
+ * Clase auxiliar encargada de realizar las distintas tareas
+ * necesarias sobre los ficheros físicos manejados por la
+ * aplicación.
+ * 
+ * @package qframe
+ * 
+ * @subpackage file
+ * 
+ * @author qinteractiva
+ *
+ */
 class Library_Qframe_File_FileUtil
 {
+	/**
+	 * Devuelve una lista con los nombres de los ficheros que
+	 * se encuentran dentro de un directorio específico.
+	 * 
+	 * Es utilizado, entre otras, en las tareas de verificación
+	 * de la existencia de un controlador concreto, ya que
+	 * mediante este método se pueden obtener los nombres de los
+	 * controladores que se encuentran dentro del directorio
+	 * en el que se ubican.
+	 * 
+	 * @param string $folder
+	 * 
+	 * 		Ruta del directorio del cual se extraerán los nombres
+	 * 		de los ficheros contienidos en él.
+	 * 
+	 * @return array
+	 * 
+	 * 		Array de cadenas de texto con los nombres de los
+	 * 		ficheros encontrados en la ubicación recibida como
+	 * 		parámetro.
+	 */
 	public static function getFilesFromFolder($folder)
 	{
 		return array_diff(scandir($folder), array('..', '.'));
 	}
 	
+	/**
+	 * Obtiene el contenido de un fichero, siendo capaz de procesar
+	 * el código PHP que se encuentra en él y devolverlo todo en
+	 * una cadena de texto.
+	 * 
+	 * Es utilizado fundamentalmente para procesar el contenido de
+	 * las plantillas empleadas dentro de la aplicación y obtener
+	 * el resultado, que posteriormente será mostrado al usuario.
+	 * 
+	 * @param string $template
+	 * 
+	 * 		Ruta de la plantilla de la cual se obtendrá el contenido.
+	 * 
+	 * @param array $view
+	 * 
+	 * 		Array que contiene variables que serán utilizadas dentro
+	 * 		del código PHP que alberga la plantilla.
+	 * 
+	 * @return
+	 * 
+	 * 		Si el proceso es correcto, devuelve el contenido de la
+	 * 		plantilla con el código PHP procesado. Si existe algún
+	 * 		error devolverá false.
+	 */
 	public static function getFileContent($template, $view)
 	{
 		if(is_file($template))
@@ -23,6 +80,15 @@ class Library_Qframe_File_FileUtil
 		}
 	}
 	
+	/**
+	 * Elimina de manera recursiva un directorio y todo el contenido
+	 * que se encuentra dentro de él.
+	 * 
+	 * @param string $dir
+	 * 
+	 * 		Ruta del directorio que se eliminará, tanto él como su
+	 * 		contenido, de la ubicación física del mismo.
+	 */
 	public static function removeDirectoryTree($dir)
 	{
 		if(file_exists($dir))
--- library/qframe/model/BaseItem.php
+++ library/qframe/model/BaseItem.php
@@ -1,9 +1,39 @@
 <?php
 
+/**
+ * Clase base de la que heredarán todos los Items utilizados por
+ * la aplicación.
+ * 
+ * Los llamados Items se trata de clases que contienen tan sólo
+ * los atributos de las entidades manejadas por la aplicación y
+ * los métodos para acceder a ellos.
+ * 
+ * Existirá por tanto un Item por cada entidad, con los atributos
+ * relacionados con la misma.
+ * 
+ * @package qframe
+ * 
+ * @subpackage model
+ * 
+ * @author qinteractiva
+ *
+ */
 abstract class Library_Qframe_Model_BaseItem
 {
+	/**
+	 * Atributo común a todos los Items, se trata del identificador
+	 * con el que son almacenados en la base de datos.
+	 * 
+	 * @var int
+	 */
 	protected $id;
 	
+	/**
+	 * Atributo común a todos los Items, se trata del objeto capaz
+	 * de representar visualmente al objeto en sus diferentes variantes.
+	 * 
+	 * @var Library_Qframe_Printer_BasePrinter
+	 */
 	protected $printer;
 	
 	public function __construct(array $options = null, Library_Qframe_Printer_BasePrinter $printer)
@@ -16,43 +46,50 @@ abstract class Library_Qframe_Model_BaseItem
 		$this->printer = $printer;
 	}
 	
-	/**
-	* Returns the id value.
-	*
-	* @return int
-	*/
-	public function getId()
-	{
-		return $this->id;
-	}
-	
-	/**
-	 * Set the id value.
-	 *
-	 * @param int $id
-	 */
-	public function setId($id)
-	{
-		$this->id = $id;
-	}
-	
 	private function setOptions(array $options)
 	{
 		$methods = get_class_methods($this);
-
+	
 		foreach($options as $key => $value)
 		{
 			$method = 'set' . ucfirst($key);
-
+	
 			if(in_array($method, $methods))
 			{
 				$this->$method($value);
 			}
 		}
-
+	
 		return $this;
 	}
 	
+	/**
+	 * Permite establecer el valor de un atributo de la clase
+	 * accediendo a ella como si fuera una propiedad pública,
+	 * pero llamando a su método set correspondiente.
+	 * 
+	 * Esto permitiría llamar al método setId() de la siguiente
+	 * manera:
+	 * 
+	 * $item->id = {valor}
+	 * 
+	 * Y equivaldrá a:
+	 * 
+	 * $item->setId({valor})
+	 * 
+	 * @param string $name
+	 * 
+	 * 		Nombre del atributo cuyo valor se modificará.
+	 * 
+	 * @param unknown_type $value
+	 * 
+	 * 		Nuevo valor que tomará el atributo especificado.
+	 * 
+	 * @throws Exception
+	 * 
+	 * 		Lanza una excepción en caso de quen no se encuentre
+	 * 		el método set que corresponde al atributo especificado.
+	 */
 	public function __set($name, $value)
 	{
 		$method = 'set' . $name;
@@ -67,6 +104,33 @@ abstract class Library_Qframe_Model_BaseItem
 		}
 	}
 	
+	/**
+	 * Permite acceder al valor de un atributo de la clase
+	 * accediendo a ella como si fuera una propiedad pública,
+	 * pero llamando a su método get correspondiente.
+	 * 
+	 * Esto permitiría llamar al método getId() de la siguiente
+	 * manera:
+	 * 
+	 * $valorId = $item->id
+	 * 
+	 * Y equivaldrá a:
+	 * 
+	 * $valorId = $item->getId()
+	 * 
+	 * @param string $name
+	 * 
+	 * 		Nombre del atributo cuyo valor se obtendrá.
+	 * 
+	 * @return unknown_type
+	 * 
+	 * 		Valor del atributo especificado.
+	 * 
+	 * @throws Exception
+	 * 
+	 * 		Lanza una excepción en caso de quen no se encuentre
+	 * 		el método set que corresponde al atributo especificado.
+	 */
 	public function __get($name)
 	{
 		$method = 'get' . $name;
@@ -79,6 +143,26 @@ abstract class Library_Qframe_Model_BaseItem
 		{
 			return $this->$method();
 		}
+	}
+	
+	/**
+	 * Devuelve el valor del atributo id.
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	/**
+	 * Establece el valor del atributo id.
+	 *
+	 * @param int $id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
 	}
 	
 	/**
@@ -101,10 +185,87 @@ abstract class Library_Qframe_Model_BaseItem
 	    $this->printer = $printer;
 	}
 	
+	/**
+	 * Devuelve los atributos del Item como un array asociativo
+	 * en el que las claves son el nombre de los mismos y los
+	 * valores los establecidos para el propio objeto.
+	 * 
+	 * Así un Item Usuario con los siguientes atributos y valores:
+	 * 
+	 * <ul>
+	 * <li>nombre: juan</li>
+	 * <li>apellidos: pérez garcía</li>
+	 * </ul>
+	 * 
+	 * Devolverá en la llamada de este método:
+	 * 
+	 * array[('nombre') => 'juan', ('apellidos') => 'pérez garcía']
+	 * 
+	 * Este método se emplea fundamentalmente para cubrir los campos
+	 * de un formulario en base a la información de un objeto recuperado
+	 * de la base de datos.
+	 * 
+	 * Así el formulario de actualización del Item Usuario será cubierto
+	 * con los datos iniciales de manera sencilla una vez recuperado el
+	 * usuario adecuado.
+	 * 
+	 * Este método ha de redefinirse en cada clase hija, aunque será
+	 * idéntico para todas ellas, por lo que simplemente será necesario
+	 * un copy-paste del mismo.
+	 * 
+	 * @return
+	 * 
+	 * 		Array asociativo con los atributos del Item y sus valores.
+	 * 
+	 */
 	public abstract function getAttributesAsArray();
 	
+	/**
+	 * Establece los valores de los atributos del Item en base a un
+	 * array asociativo recibido como parámetro, en el que las claves
+	 * son el nombre de los mismos y los valores asociados a ellas
+	 * los que se establecerán para cada uno de los atributos.
+	 *
+	 * Así un Item Usuario con los siguientes atributos y valores:
+	 *
+	 * <ul>
+	 * <li>nombre</li>
+	 * <li>apellidos</li>
+	 * </ul>
+	 *
+	 * Al que sea invocado este método con los siguientes valores como
+	 * parámetro:
+	 *
+	 * array[('nombre') => 'juan', ('apellidos') => 'pérez garcía']
+	 * 
+	 * Actualizará los valores de sus atributos a los contenidos en él.
+	 *
+	 * Este método se emplea fundamentalmente para cubrir los campos
+	 * de un objeto en base a la información introducida por el usuario
+	 * a través de un formulario
+	 *
+	 * Este método ha de redefinirse en cada clase hija, aunque será
+	 * idéntico para todas ellas, por lo que simplemente será necesario
+	 * un copy-paste del mismo.
+	 *
+	 * @param array $options
+	 *
+	 * 		Array asociativo con los atributos y los valores que se
+	 * 		establecerán para el Item sobre el que es invocado este
+	 * 		método.
+	 *
+	 */
 	public abstract function setAttributesFromArray(array $options);
 	
+	/**
+	 * Desvía la salida estándar del objeto a la salida
+	 * estándar definida en su atributo printer.
+	 * 
+	 * @return
+	 * 
+	 * 		Representación base del objeto, definida por
+	 * 		su atributo printer.
+	 */
 	public function __toString()
 	{
 		return $this->printer->standardPrint();
